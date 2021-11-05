075f439dcb22f1dcda9f0d837b38ef3422335722
Giovanni Toffoli <toffoli@linkroma.it>
2021-11-05 23:45:12 +0100

    Added settings module.

---
+++ success/settings.py
@@ -0,0 +1,21 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(__file__)
+
+HAS_SAML2 = False # supports the SSO interface provided by the Up2U project (www.up2uiversity.eu) ?
+HAS_EARMASTER = False # supports import and processing of the data exported from the EarMaster application ?
+PRIMARY_DOMAIN = 'success4all.commonspaces.eu'
+SECONDARY_DOMAIN = None
+TEST_DOMAIN = None
+
+from commons.settings import *
+
+SITE_ID = 4
+SITE_NAME = 'SUCCESSxALL'
+
+WSGI_APPLICATION = 'health.wsgi.application'
+ROOT_URLCONF = 'success.urls'
+
+PROJECT_TITLE = 'SUCCESSxALL - Supporting success for all people'
+PROJECT_NAME = 'success'
+LOGIN_REDIRECT_URL = 'success.home'
